dd734dc7820e0ef183000f46906d43b468999b22
AWFX <94919004+AWFX@users.noreply.github.com>
2023-07-24 14:03:07 +0500

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -4,16 +4,16 @@
 # Установка PostgreSQL
 #
 #
-wget https://repo.postgrespro.ru/1c-13/keys/pgpro-repo-add.sh 				# Получение скрипта, который добавляет репозитории
+wget https://repo.postgrespro.ru/1c-12/keys/pgpro-repo-add.sh 				# Получение скрипта, который добавляет репозитории
 sh pgpro-repo-add.sh
 #
-apt-get install -y postgrespro-1c-13							# установка СУБД PostgreSQL
+apt-get install -y postgrespro-1c-12							# установка СУБД PostgreSQL
 rm -rf pgpro-repo-add.sh								# удаление скрипта
 #
-rm -rf /var/lib/pgpro/1c-13/data/ 							# удаление тестовой базы данных
-/opt/pgpro/1c-13/bin/pg-setup initdb --tune=1c --locale=ru_RU.UTF-8 			# инициализация новой базы с нужными параметрами
-rm -f /var/lib/pgpro/1c-13/data/postgresql.conf						# удаление старого файла конфигураций
-cp /mnt/postgresql.conf /var/lib/pgpro/1c-13/data/ 					# перемещение нового файла конфигураций
+rm -rf /var/lib/pgpro/1c-12/data/ 							# удаление тестовой базы данных
+/opt/pgpro/1c-12/bin/pg-setup initdb --tune=1c --locale=ru_RU.UTF-8 			# инициализация новой базы с нужными параметрами
+rm -f /var/lib/pgpro/1c-12/data/postgresql.conf						# удаление старого файла конфигураций
+cp /mnt/postgresql.conf /var/lib/pgpro/1c-12/data/ 					# перемещение нового файла конфигураций
 #
 #
 systemctl enable postgrespro-1c-12							# добавление PostgreSQL в автозагрузку
@@ -37,8 +37,8 @@ dpkg -i /mnt/*.deb									# установка пакетов
 #
 ln -s /opt/1cv8/x86_64/*/srv1cv83 /etc/init.d/srv1cv83					# создание сивольной ссылки, для удобного обращения к сервису 1С
 #
-systemctl enable /opt/1cv8/x86_64/*/srv1cv83 						# добавление серсиса в автозагрузку
-systemctl start /opt/1cv8/x86_64/*/srv1cv83						# запуск сервиса 1С
+systemctl enable srv1cv83 		                				# добавление серсиса в автозагрузку
+systemctl start srv1cv83						                  # запуск сервиса 1С
 #
 rm -rf /mnt/*.deb									# удаление пакетов
 #
